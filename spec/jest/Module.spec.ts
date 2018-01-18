d5f64c4a26c1300b271187f7ec255daf688ef52f
Dusan Perkovic <dperkovic@Dusans-MacBook-Pro.local>
2018-01-18 10:58:22 +0100

    Implemented tests for Module

---
+++ spec/jest/Module.spec.ts
@@ -0,0 +1,30 @@
+import Module from '../../src/new/Module';
+import Sandbox from '../../src/new/Sandbox';
+
+describe('Module', () => {
+
+    it('should be instance of Module', () => {
+		const module = new Module(document.createElement('div'), new Sandbox(null));
+        expect(module instanceof Module).toBeTruthy();
+    });
+
+    it('should set instance variables', () => {
+        const ctx = document.createElement('div');
+        const sandbox = new Sandbox(null);
+        const module = new Module(ctx, sandbox);
+
+        expect(module._ctx).toBe(ctx);
+        expect(module._sandbox).toBe(sandbox);
+    });
+
+    describe('#start()', () => {
+        it('should not throw an error if the start and stop methods are missing', function () {
+			const module = new Module(document.createElement('div'), new Sandbox(null));
+
+            expect(() => {
+                module.start(() => {});
+            }).not.toThrow();
+        });
+    });
+});
+
