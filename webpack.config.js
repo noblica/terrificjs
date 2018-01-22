31096b341d8d15b1685d465162f172dd3d0c0f2e
Dusan Perkovic <dperkovic@Dusans-MacBook-Pro.local>
2018-01-22 17:14:49 +0100

    Added minimization to webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/new/Utils.ts',
+  entry: './src/new/exports.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist/new')
